49beb7f1ac38dd85b03ae7500f561591b6d47747
justalittlenoob <330631390@qq.com>
2015-09-25 18:22:06 +0800

    [add]pic; test setup

---
+++ tests/setup/h.py
@@ -0,0 +1,3 @@
+class h2(object):
+    def __init__(self,s):
+        print s
---
+++ tests/setup/hello.py
@@ -0,0 +1,8 @@
+if __name__ =='__main__':
+    import h
+    
+    count=0
+    while 1:
+        m=h.h2('zpf')
+        print "hello", count
+        count += 1
